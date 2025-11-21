f1584fb42746506dc5ab63a6d44d179348d04fa3
gabxdev1 <gabriel.duraes@alunos.unis.edu.br>
2025-11-20 23:41:34 -0300

    listener bolado

--- src/main/java/br/com/gabxdev/infra/adapter/in/web/SqsListenerAdminController.java
+++ src/main/java/br/com/gabxdev/infra/adapter/in/web/SqsListenerAdminController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/sqs-listener")
-@Slf4j
 public class SqsListenerAdminController implements PollerConfigPort {
 
     private int desiredNumPollers = 0;
